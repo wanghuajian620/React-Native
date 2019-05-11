845310a58934bb954852fc2c1333961ce5371acf
wanghuajian620 <154659041@qq.com>
2019-05-11 13:06:49 +0800

    加入启动页，基本功能完成。

--- alice/ios/alice/AppDelegate.m
+++ alice/ios/alice/AppDelegate.m
@@ -27,6 +27,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [NSThread sleepForTimeInterval:3.0];
   return YES;
 }
 
