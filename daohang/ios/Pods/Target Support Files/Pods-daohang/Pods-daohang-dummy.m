60bf891d533c8fda9af8d16a68d3f112e3652fb3
wanghuajian620 <154659041@qq.com>
2020-04-24 10:43:38 +0800

    why so much files

---
+++ daohang/ios/Pods/Target Support Files/Pods-daohang/Pods-daohang-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_daohang : NSObject
+@end
+@implementation PodsDummy_Pods_daohang
+@end
--- daohang/ios/daohang/AppDelegate.m
+++ daohang/ios/daohang/AppDelegate.m
@@ -10,11 +10,15 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <AVFoundation/AVFoundation.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:nil];  // allow
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"daohang"
